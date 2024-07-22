525b166ba9738baa5012d36ded0881303837aeb5
Andre-crtl <andre.schellenberg@edu.escp.eu>
2024-07-22 13:18:16 +0200

    add_purchase_cost
    added a purchase cost column by date_date

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,5 +1,8 @@
+#add_purchase_cost
 SELECT 
-date_date
-,SUM(turnover) as turnover_total
+date_date 
+    ,ROUND(SUM(turnover),2) as turnover_total
+    ,ROUND(SUM(purchase_cost),2) as purchase_cost_total
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
 GROUP BY date_date
+
