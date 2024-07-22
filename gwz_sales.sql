4945b74cba4b4e3487b12c4b25342369bfc30683
Andre-crtl <andre.schellenberg@edu.escp.eu>
2024-07-22 15:24:18 +0200

    develop 02
    Order by date_date

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,8 +1,8 @@
-# take out the rounding
+# Order date_date by DESC
 SELECT 
-date_date 
+    date_date 
     ,SUM(turnover) as turnover_total
     ,SUM(purchase_cost) as purchase_cost_total
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
 GROUP BY date_date
-
+ORDER BY date_date DESC
